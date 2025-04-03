a97b98797bc67049b3bf41639f08df7739925d35
Harsh <haryan998@gmail.com>
2025-04-03 18:23:22 +0530

    New Tour and All Tours.

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+///GET FOR ALL TOURS
 app.get("/api/v1/tours", (req, res) => {
   res.status(200).json({
     status: "success",
@@ -18,9 +19,36 @@ app.get("/api/v1/tours", (req, res) => {
   });
 });
 
+///GET FOR A TOUR BY ID
+
+app.get("/api/v1/tours/:id", (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour,
+    },
+  });
+});
+
 app.post("/api/v1/tours", (req, res) => {
-  console.log(req.body);
-  res.send("Done");
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: "success",
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const PORT = 3000;
