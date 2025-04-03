087ff8cb0ddf1485af440a10837300232988f6fa
Harsh <haryan998@gmail.com>
2025-04-03 18:30:44 +0530

    For Invalid ID

--- app.js
+++ app.js
@@ -23,6 +23,12 @@ app.get("/api/v1/tours", (req, res) => {
 
 app.get("/api/v1/tours/:id", (req, res) => {
   const id = req.params.id * 1;
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: "Fail",
+      message: "Invalid ID",
+    });
+  }
   const tour = tours.find((el) => el.id === id);
   res.status(200).json({
     status: "success",
