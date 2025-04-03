fd53abb82d632a0f6038f61dcdf50c7597ee6038
Harsh <haryan998@gmail.com>
2025-04-03 17:48:27 +0530

    starting structure of the Tour Project.

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+app.use(express.json());
+
+const tours = JSON.parse(
+  ////// ARRAY of Tours //////
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+  console.log(req.body);
+  res.send("Done");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}...`);
+});
