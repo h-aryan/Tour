50b4a9b2591bde7cbb20aa111b394fdc598b438a
Harsh <haryan998@gmail.com>
2025-04-03 23:16:29 +0530

    Patch Structure Update in code.

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -7,20 +7,25 @@ import User from "./../../models/userModel";
 
 dotenv.config({ path: "./config.env" });
 
-const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD);
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
 
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false
+    useFindAndModify: false,
   })
   .then(() => console.log("DB connection successful!"));
 
 // READ JSON FILE
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, "utf-8"));
 const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, "utf-8"));
-const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`, "utf-8"));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, "utf-8")
+);
 
 // IMPORT DATA INTO DB
 const importData = async () => {
