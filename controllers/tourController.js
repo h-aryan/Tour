f64a82f5fe10c6364eaabcbe15d5aa9ddc02a820
Harsh <haryan998@gmail.com>
2025-04-10 19:24:33 +0530

    Successfull adding and retrieving of data from DB.

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -5,42 +5,65 @@ const Tour = require("./../model/tourModel");
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );*/
 
-exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: "success",
-    requestedAT: req.requestTime,
-    /*results: tours.length,
-    data: {
-      tours,
-    },*/
-  });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: "success",
+      requestedAT: req.requestTime,
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }
 };
 
-exports.getTour = (req, res) => {
-  const id = req.params.id * 1;
-  /*if (id > tours.length) {
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+
+    /*if (id > tours.length) {
     return res.status(404).json({
       status: "Fail",
       message: "Invalid ID",
     });
   }
-  const tour = tours.find((el) => el.id === id);
-  res.status(200).json({
-    status: "success",
-    data: {
-      tour,
-    },
-  });*/
+  const tour = tours.find((el) => el.id === id);*/
+    res.status(200).json({
+      status: "success",
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }
 };
 
-exports.newTour = (req, res) => {
-  res.status(200).json({
-    status: "success",
-    /*data: {
-      tour,
-    },*/
-  });
-
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: "success",
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
   /*const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
   tours.push(newTour);
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -2,7 +2,10 @@ const express = require("express");
 const tourController = require("./../controllers/tourController");
 const router = express.Router();
 
-router.route("/").get(tourController.getAllTours).post(tourController.newTour);
+router
+  .route("/")
+  .get(tourController.getAllTours)
+  .post(tourController.createTour);
 router
   .route("/:id")
   .get(tourController.getTour)
