d47c9d244133fb684083bf4cc76a8f86cbdc7206
Harsh <haryan998@gmail.com>
2025-06-14 17:05:06 +0530

    building home page part-1

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,7 +1,15 @@
-exports.getOverview = (req, res) => {
+const Tour = require("../model/tourModel");
+
+exports.getOverview = async (req, res, next) => {
+  // 1) Get all tours from the database (mocked here)
+  const tours = await Tour.find();
+  //2) Build template based on the data
+
+  // 3) Render that template using the data from step 1
   res.status(200).render("overview", {
     title: "All Tours",
     message: "Welcome to the Overview Page!",
+    tours: tours,
   });
 };
 
